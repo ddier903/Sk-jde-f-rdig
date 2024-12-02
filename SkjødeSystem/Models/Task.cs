a646bdba94ceb71ab77283c2ca7b7e961f4a3b4c
Frede52 <frederikkarno@gmail.com>
2024-12-02 23:11:06 +0100

    Added to task class

--- SkjødeSystem/Models/Task.cs
+++ SkjødeSystem/Models/Task.cs
@@ -4,15 +4,17 @@
     {
         public int TaskId { get; set; }
         public string TaskName { get; set; }
-        public string Image { get; set; }
-        public string Description { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string Status { get; set; }
-        public string Comment { get; set; }
+        public string Image { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime? EndDate { get; set; }
+        public string Status { get; set; } = "Pending";
+        public string Comment { get; set; } = String.Empty;
         public int SubcontractorID { get; set; }
         public int AdminId { get; set; }
-        public string ToDoPDF { get; set; }
+        public string ToDoPDF { get; set; } = String.Empty;
 
     }
+
+
 }
