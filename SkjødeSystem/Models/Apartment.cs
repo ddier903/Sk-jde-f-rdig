e58a24dd8dc33f034ec453a126f4ddcbeda6163f
Frede52 <frederikkarno@gmail.com>
2024-12-07 13:09:35 +0100

    Ændringer til apartment class

--- SkjødeSystem/Models/Apartment.cs
+++ SkjødeSystem/Models/Apartment.cs
@@ -3,10 +3,6 @@
     public class Apartment
     {
         public int ApartmentId { get; set; }
-        public string Address { get; set; }
-        public string Status { get; set; } = "Ikke Fuldført";
-
-        public Tenant tenant { get; set; }
-        public List<Availability> Availability { get; set; }
+        public string Address {  get; set; }
     }
 }
