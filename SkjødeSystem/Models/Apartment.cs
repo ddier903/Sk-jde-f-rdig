f518c4758f30fa7e7c83d06880846e2b9b473d55
Frede52 <frederikkarno@gmail.com>
2024-12-07 13:10:26 +0100

    dsadas

--- SkjødeSystem/Models/Apartment.cs
+++ SkjødeSystem/Models/Apartment.cs
@@ -3,6 +3,7 @@
     public class Apartment
     {
         public int ApartmentId { get; set; }
-        public string Address {  get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; } = "Ikke Fuldført";
     }
 }
