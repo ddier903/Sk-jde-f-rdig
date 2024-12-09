c8b39a50590e89e4bdb99de2ddd07dd50c65f594
Frede52 <frederikkarno@gmail.com>
2024-12-09 13:51:54 +0100

    new changes

--- ServerAPI/Controllers/TaskController.cs
+++ ServerAPI/Controllers/TaskController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServerAPI.Repositories;
 using Core;
+using System.Reflection;
+using SkjødeSystem.Models;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -14,20 +16,78 @@ public class TaskController : ControllerBase
     }
 
     // Post Task:
+    [HttpPost]
+    [Route("AddTask")]
+    public async Task<IActionResult> PostTask(TaskItem task)
+    {
+
+        await _repository.PostTask(task);
+        return Ok();
+
+    }
 
     // Update Task: 
+    [HttpPut]
+    [Route("UpdateTask")]
+    public async Task<IActionResult> UpdateTask(int taskId, TaskItem updatedtask)
+    {
+
+        await _repository.UpdateTask(taskId, updatedtask);
+        return Ok();
+
+    }
 
     // Delete Task: 
+    [HttpDelete]
+    [Route("DeleteTask")]
+    public async Task<IActionResult> DeleteTask(int taskId)
+    {
+
+        await _repository.DeleteTask(taskId);
+        return Ok();
+
+    }
 
     //Get Task by ApartmentID:
+    [HttpGet]
+    [Route("GetTaskByApartmentId")]
+    public async Task<IEnumerable<TaskItem>> GetAllTasksByApartmentId(int apartmentId)
+    {
+        return await _repository.GetAllTaskByApartmentId(apartmentId);
+    }
+
 
     //Get Task by ApartmentID, sorted by Status: 
 
     //Get Task by Subconctractor:
+    [HttpGet]
+    [Route("GetTaskBySubcontractor")]
+    public async Task<IEnumerable<TaskItem>> GetAllTasksBySubcontractor(int userId)
+    {
+        return await _repository.GetAllTasksBySubcontractor(userId);
+    }
 
     // Get all Tasks:
+    [HttpGet]
+    [Route("GetAllTasks")]
+    public async Task<IEnumerable<TaskItem>> GetAllTasks() 
+    {
+        return await _repository.GetAllTasks();
+    }
 
-    // Get Task: 
+    // Get Task by Id: 
+    [HttpGet]
+    [Route("GetTasksById")]
+    public async Task<TaskItem> GetTaskById(int taskId)
+    {
+        return await _repository.GetTaskById(taskId);
+    }
 
-    // Get Task by status:
+    // Get Tasks filtered by status:
+    [HttpGet]
+    [Route("GetTasksByStatus")]
+    public async Task<IEnumerable<TaskItem>> FilterTaskByStatus(string status)
+    {
+        return await _repository.FilterTaskByStatus(status);
+    }
 }
--- ServerAPI/Repositories/TaskRepository.cs
+++ ServerAPI/Repositories/TaskRepository.cs
@@ -20,19 +20,67 @@ public class TaskRepository
 
     // Post Task:
 
+    public async Task PostTask(TaskItem task)
+    {
+        await collection.InsertOneAsync(task);
+    }
+
     // Update Task: 
+
+    public async Task UpdateTask(int taskId, TaskItem updatedtask)
+    {
+        var filter = Builders<TaskItem>.Filter.Eq("TaskId", taskId);
+
+        var update = Builders<TaskItem>.Update
+       .Set(task => task.Image, updatedtask.Image)
+       .Set(task => task.Status, updatedtask.Status)
+       .Set(task => task.EndDate , updatedtask.EndDate)
+       .Set(task => task.Comment, updatedtask.Comment);
+    }
 
     // Delete Task: 
 
-    //Get Task by ApartmentID:
+    public async Task DeleteTask(int taskId)
+    {
+        var filter = Builders<TaskItem>.Filter.Eq("TaskId", taskId);
+        await collection.DeleteOneAsync(filter);
+    }
+
+    //Get All Task by ApartmentID:
+    public async Task<List<TaskItem>> GetAllTaskByApartmentId(int id)
+    {
+        var filter = Builders<TaskItem>.Filter.Eq("Apartment.ApartmentId", id);
+        return await collection.Find(filter).ToListAsync();
+    }
 
     //Get Task by ApartmentID, sorted by Status: 
 
-    //Get Task by Subconctractor:
+
+    //Get All Task by Subconctractor:
+    public async Task<List<TaskItem>> GetAllTasksBySubcontractor(int id)
+    {
+        var filter = Builders<TaskItem>.Filter.Eq("Subcontractor.UserId", id);
+        return await collection.Find(filter).ToListAsync();
+    }
 
     // Get all Tasks:
+    public async Task<List<TaskItem>> GetAllTasks()
+    {
+        var filter = Builders<TaskItem>.Filter.Empty;
+        return await collection.Find(filter).ToListAsync();
+    }
 
-    // Get Task: 
+    // Get Task by Id: 
+    public async Task<List<TaskItem>> GetTaskById(int id)
+    {
+        var filter = Builders<TaskItem>.Filter.Eq("TaskId", id);
+        return await collection.Find(filter).ToListAsync();
+    }
 
-    // Get Task by status:
+    // Get All Task by status:
+    public async Task<List<TaskItem>> FilterTaskByStatus(string status)
+    {
+        var filter = Builders<TaskItem>.Filter.Eq("Status", status);
+        return await collection.Find(filter).ToListAsync();
+    }
 }
