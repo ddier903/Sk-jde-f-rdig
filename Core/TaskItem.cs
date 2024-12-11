858f90b8ad9617c3e2c47b46a58c48011f86811e
Frede52 <frederikkarno@gmail.com>
2024-12-11 13:32:50 +0100

    added new page

--- Core/TaskItem.cs
+++ Core/TaskItem.cs
@@ -17,7 +17,7 @@ namespace Core
         public string[]? Image { get; set; }
         [Required]
         public string Description { get; set; } = string.Empty;
-        public DateTime StartDate { get; set; } 
+        public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime? EndDate { get; set; }
         public string Status { get; set; } = "Pending";
         public string? Comment { get; set; } = string.Empty;
--- Core/User.cs
+++ Core/User.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core
 {
@@ -11,9 +12,13 @@ namespace Core
         [BsonRepresentation(BsonType.ObjectId)]
         public ObjectId Id { get; set; } 
         public int UserId { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string Email { get; set; }
         public string Role { get; set; }
     }
