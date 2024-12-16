811a2c823a297e314aad6255d785f2eef24682aa
Frede52 <frederikkarno@gmail.com>
2024-12-16 12:18:11 +0100

    New changes

--- Core/User.cs
+++ Core/User.cs
@@ -19,9 +19,11 @@ namespace Core
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
         public string Phone { get; set; }
         [Required]
         public string Email { get; set; }
+        [Required]
         public string Role { get; set; }
     }
 }
