78907ff25d5cd6d3b6b56d95c4180411179853b3
Giobeshara <giobe@DESKTOP-2CE88N8>
2024-12-16 20:36:59 +0100

    Teste

--- SkjødeSystem/Services/IApartmentService.cs
+++ SkjødeSystem/Services/IApartmentService.cs
@@ -7,5 +7,14 @@ namespace SkjødeSystem.Services
     public interface IApartmentService
     {
         Task<List<Apartment>> GetAllApartments();
+        Task<Apartment> GetApartmentById(string id);
+        Task<List<Apartment>> GetApartmentsByStatus(string status);
+        Task<Apartment> AddApartment(Apartment apartment);
+        Task<Apartment> UpdateApartment(string id, Apartment updatedApartment);
+        Task<long> GetApartmentsNotFinishedCount();
+        Task<Apartment> GetApartmentByUserId(string userId);
+        Task<bool> AssignTenantToApartment(string apartmentId, Tenant tenant);
+        Task<bool> UpdateApartmentAvailability(string apartmentId, List<Availability> availabilities);
+        Task<List<Availability>> GetAvailabilityByApartmentId(string apartmentId);
     }
 }
